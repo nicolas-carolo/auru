825f00a762eb2da7e2b8a1f608a3be93e0bf9dc7
Nicolas Carolo <nicolas.carolo@outlook.com>
2020-03-16 22:50:35 +0100

    .aur_tbu

--- auru.sh
+++ auru.sh
@@ -7,19 +7,35 @@ PREFIX_LEN=${#PREFIX}
 
 # TODO check if the folder aur exists
 
+function user_says_no_to_update() {
+	if ! [ -f "./.aur_tbu" ]; then
+		touch .aur_tbu
+	fi
+}
+
+
+function user_says_yes_to_update() {
+        if [ -f "./.aur_tbu" ]; then
+		rm .aur_tbu
+		git pull
+	fi
+	makepkg -si
+}
+
+
 for folder in $AUR_PATH/*/ ; do
 	cd $folder
 	git_output=$(git pull)
 	sw_name=${folder:$PREFIX_LEN:-1}
-	if [ "$git_output" == "Already up to date." ] ; then
+	if [ "$git_output" == "Already up to date." ] && ! [ -f "./.aur_tbu" ]  ; then
 		echo "$sw_name is up-to-date"
 	else
 		echo "$sw_name is not up-to-date"
-		read -p "Do you wish to upgrade '$folder'? " yn
+		read -p "Do you wish to upgrade '$sw_name'? " yn
 		case $yn in
-			[Yy]* ) makepkg -si;;
-			[Nn]* ) ;;
-			* ) echo "$sw_name: upgrade aborted"
+			[Yy]* ) users_says_yes_to_update;;
+			[Nn]* ) user_says_no_to_update;;
+			* ) user_says_no_to_update; echo "$sw_name: upgrade aborted"
 		esac
 	fi
 	cd ..
